7d3ae0c105af58f3b6bd27ddcd1c4e65c688242c
koen van vliet <8by8mail@gmail.com>
2025-07-31 09:50:58 +0200

    Add DIG:OUT command

--- arduino/scpi/scpi.ino
+++ arduino/scpi/scpi.ino
@@ -28,17 +28,22 @@
 #define SW_VERSION "1.0"
 
 #define BUFFER_SIZE 256
-#define LED_PIN 9
+#define LED_PIN 10
+
+#define error(e) last_error = e; errorBeep(); Serial.println(e);
 
 typedef enum{
   NO_ERROR = 0,
   COMMAND_ERROR = -100,
+  INVALID_CHARACTER_ERROR = -101,
+  PARAMETER_ERROR = -104,
   INTERNAL_FIRMWARE_ERROR = -300,
 }Error;
 
 const char cmdIdn[] = "*IDN?";
 const char cmdVolt[] = "MEAS:VOLT:DC?";
 const char cmdErr[] = "SYST:ERR?";
+static const char cmdDigOut[] = "DIG:OUT";
 
 uint32_t deviceSignature; // Unique to the type of microcontroller used
 uint32_t serialNumberL;
@@ -77,7 +82,7 @@ void errorBeep(void){
   /// TODO * loud beep noise *
   int i;
 
-  for (i = 0; i < 10; i++) {
+  for (i = 0; i < 2; i++) {
     digitalWrite(LED_PIN, HIGH);
     delay(100);
     digitalWrite(LED_PIN, LOW);
@@ -103,39 +108,96 @@ void loop() {
   static char command[BUFFER_SIZE];
   static int16_t last_error = NO_ERROR;
   char *p = command;
+  char c;
   int i;
   
   // put your main code here, to run repeatedly:
-  if (Serial.available()){
-    while (Serial.available()) {
-      *p = Serial.read(); 
-      p++;
+  while (Serial.available()){
+    if (i < (BUFFER_SIZE - 1)) {
+      c = Serial.read();
+      command[i] = c;
+      i++;
     }
-    *p = 0; // Terminate the string
 
-    if (startswith(cmdErr, command)) {
-      Serial.println(last_error);
-      last_error = NO_ERROR;
-    } else if (startswith(cmdIdn, command)) {
-      Serial.print("Arduino,0x");
-      Serial.print(deviceSignature,HEX);
-      Serial.print(",0x");
-      Serial.print(serialNumberH,HEX);
-      Serial.print(serialNumberL,HEX);
-      Serial.print(",");
-      Serial.println(SW_VERSION);
-    } else if (startswith(cmdVolt, command)) {
-      float value = (5.0 / 1024) * analogRead(A0);
-      
-      if (dtostre(value, command, 8, 0)) {
-        Serial.println(command);
+    // When a newline is encountered, parse and execute the command
+    if (c == '\n') {
+      command[i] = 0; // Append NULL character to string
+      i = 0; // Reset write head to beginning of string.
+      p = command;
+
+      if (startswith(cmdErr, command)) {
+        Serial.println(last_error);
+        last_error = NO_ERROR;
+      } else if (startswith(cmdIdn, command)) {
+        Serial.print("Arduino,0x");
+        Serial.print(deviceSignature,HEX);
+        Serial.print(",0x");
+        Serial.print(serialNumberH,HEX);
+        Serial.print(serialNumberL,HEX);
+        Serial.print(",");
+        Serial.println(SW_VERSION);
+      } else if (startswith(cmdDigOut, command)){
+        
+        p = p + sizeof(cmdDigOut)-1;
+        while (*p == ' ') {
+          p ++;
+        }
+
+        if ((*p != '(') | (*(p + 1) != '@')) {
+          Serial.println("no @ or ()");
+          error(COMMAND_ERROR);
+          break;
+        }
+
+        p+=2;
+
+        bool state;
+        int pin = *p - 'A';
+        p++;
+        
+        if (*p != ')') {
+          Serial.println("no )");
+          error(COMMAND_ERROR);
+          break;
+        }
+        p++;
+          
+        if ((pin < 0) || (pin >= 4 )) {
+          Serial.println("pin no not ok");
+          error(PARAMETER_ERROR);
+          break;
+        }
+          
+        while (*p == ' ') {
+          p ++;
+        }
+        
+        
+        if (*p == '1') {
+          state = 1;
+        } else if (*p == '0') {
+          state = 0;
+        } else {
+          Serial.println("invalid state");
+          error(INVALID_CHARACTER_ERROR);
+          break;
+        }
+        
+        pinMode(pin, OUTPUT);
+        digitalWrite(pin, state);
+      } else if (startswith(cmdVolt, command)) {
+        float value = (5.0 / 1024) * analogRead(A0);
+        
+        if (dtostre(value, command, 8, 0)) {
+          Serial.println(command);
+        } else {
+          error(INTERNAL_FIRMWARE_ERROR);
+          break;
+        }
       } else {
-        last_error = INTERNAL_FIRMWARE_ERROR;
-        errorBeep();
+        error(COMMAND_ERROR);
+        break;
       }
-    } else {
-      last_error = COMMAND_ERROR;
-      errorBeep();
     }
   }
 }
